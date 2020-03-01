ca68bc966c2d3175e1761c90d1a1080c0a3028b2
Łukasz <riraseth@gmail.com>
2020-03-01 19:55:20 +0100

    cart

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,1 +1,17 @@
-// cart context
+import React, { useState } from 'react';
+import localCart from '../utils/localCart';
+import { createContext } from 'react';
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState(localCart);
+  const [total, setTotal] = useState(0);
+  const [cartItems, setCartItems] = useState(0);
+  return (
+    <CartContext.Provider value={{ cart, total, cartItems }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
