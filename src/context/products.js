94bb03182fd8712ffb9a65a62155c333ef73cd27
Łukasz <riraseth@gmail.com>
2020-03-01 17:08:06 +0100

    products context

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,20 @@
-// products context
+import React, { createContext } from 'react';
+
+export const ProductContext = createContext();
+
+//provider
+
+const ProductProvider = ({ children }) => {
+  const greeting = 'hello';
+  const product = {
+    id: 1,
+    title: 'samsung'
+  };
+  return (
+    <ProductContext.Provider value={{ greeting, ...product }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ProductContext } from '../context/products';
 
 export default function Products() {
+  const { greeting, id, title } = useContext(ProductContext);
+  console.log(greeting, id, title);
   return <h1>hello from products page</h1>;
 }
