8ee1a18136a6ec3994c987b8424f86688f669535
Łukasz <riraseth@gmail.com>
2020-03-01 16:48:00 +0100

    home,error,about

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,33 @@
-import React from "react";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 export default function Header() {
-  return <h1>hello from header</h1>;
+  return (
+    <header className="header">
+      <img src={logo} alt="" className="logo" />
+      <nav>
+        <ul>
+          <div>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+          </div>
+          <div>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Bits of moving fluff the carbon in our apple pies Tunguska event invent
+        the universe culture descended from astronomers. Two ghostly white
+        figures in coveralls and helmets are soflty dancing a still more
+        glorious dawn awaits Cambrian explosion from which we spring
+        extraordinary claims require extraordinary evidence concept of the
+        number one? A mote of dust suspended in a sunbeam finite but unbounded
+        finite but unbounded how far away brain is the seed of intelligence
+        encyclopaedia galactica and billions upon billions upon billions upon
+        billions upon billions upon billions upon billions.
+      </p>
+    </section>
+  );
 }
