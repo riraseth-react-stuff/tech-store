9ffbf30dc673dcb57dbc8a5eaa5d662b6ffcdfba
Łukasz <riraseth@gmail.com>
2020-03-07 23:32:54 +0100

    filtering setup

--- src/context/products.js
+++ src/context/products.js
@@ -4,25 +4,52 @@ import url from '../utils/URL';
 import { featuredProducts, flattenProducts } from '../utils/helpers';
 export const ProductContext = createContext();
 
-//provider
-
 const ProductProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [featured, setFeatured] = useState([]);
+  const [sorted, setSorted] = useState([]);
+  const [page, setPage] = useState(0);
+  const [filter, setFilter] = useState({
+    search: '',
+    category: 'all',
+    shipping: false,
+    price: 'all'
+  });
+
+  const changePage = index => {
+    console.log(index);
+  };
+
+  const updateFilters = e => {
+    console.log(e);
+  };
+
   useEffect(() => {
     setLoading(true);
     axios.get(`${url}/products`).then(response => {
       // const featProds = featuredProducts(response.data);
       const featProds = featuredProducts(flattenProducts(response.data));
       const products = flattenProducts(response.data);
+      setSorted(products);
       setProducts(products);
       setFeatured(featProds);
       setLoading(false);
     });
   }, []);
   return (
-    <ProductContext.Provider value={{ products, loading, featured }}>
+    <ProductContext.Provider
+      value={{
+        products,
+        loading,
+        featured,
+        sorted,
+        page,
+        filter,
+        changePage,
+        updateFilters
+      }}
+    >
       {children}
     </ProductContext.Provider>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,9 +3,9 @@ import { ProductContext } from '../context/products';
 import Loading from '../components/Loading';
 import ProductList from '../components/Products/ProductList';
 export default function Products() {
-  const { loading, products } = useContext(ProductContext);
+  const { loading, sorted } = useContext(ProductContext);
   if (loading) {
     return <Loading></Loading>;
   }
-  return <ProductList title="our products" products={products}></ProductList>;
+  return <ProductList title="our products" products={sorted}></ProductList>;
 }
